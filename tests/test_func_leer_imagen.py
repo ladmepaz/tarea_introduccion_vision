e0775026c647add1c0b14f5172e64f7aa3c7fdd7
JoseFRuiz <jfruizmu@gmail.com>
2024-10-23 19:40:48 -0500

    update code

--- tests/test_func_leer_imagen.py
+++ tests/test_func_leer_imagen.py
@@ -3,7 +3,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_leer_imagen():
-    from codigo_profesor import leer_imagen
+    from codigo_estudiante import leer_imagen
     ruta_imagen = os.path.join("data","imagen0.png")
     img = leer_imagen(ruta_imagen)
     assert img.size[0] == 1024

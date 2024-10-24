da7383601a7961c7130fa14f573610fce8fe9c94
JoseFRuiz <jfruizmu@gmail.com>
2024-10-23 21:19:01 -0500

    Corrección funciones

--- tests/.ipynb_checkpoints/test_func_estadisticas_por_canal-checkpoint.py
+++ tests/.ipynb_checkpoints/test_func_estadisticas_por_canal-checkpoint.py
@@ -1,5 +1,8 @@
 import sys
 import os
+import numpy as np
+from PIL import Image
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_estadisticas_por_canal():

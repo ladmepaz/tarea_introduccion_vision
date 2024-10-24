1b5bc013e60d14d09d34e258ecf9548517fbed15
JoseFRuiz <jfruizmu@gmail.com>
2024-10-23 21:20:46 -0500

    Actualización función

--- tests/.ipynb_checkpoints/test_func_estadisticas_por_canal-checkpoint.py
+++ tests/.ipynb_checkpoints/test_func_estadisticas_por_canal-checkpoint.py
@@ -6,7 +6,7 @@ from PIL import Image
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_estadisticas_por_canal():
-    from codigo_estudiante import estadisticas_intensidad
+    from codigo_estudiante import estadisticas_por_canal
     ruta_imagen = os.path.join("data","imagen0.png")
     img = Image.open(ruta_imagen)
     arreglo_img = np.array(img)
